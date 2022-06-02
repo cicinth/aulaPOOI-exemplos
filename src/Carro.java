a70f88bcd4a76972bb7828b062edbd245af1ea9c
cicinth <cinthiaqcardoso@hotmail.com>
2022-06-02 16:50:02 -0300

    update code

--- src/Carro.java
+++ src/Carro.java
@@ -3,4 +3,29 @@ public class Carro {
     public String modelo;
     public String cor;
     public String placa;
+    public int velocidadeAtual;
+    public boolean estaLigado;
+
+    public void ligar(){
+        if(this.estaLigado){
+            System.out.println("O carro ja esta ligado");
+        } else {
+            System.out.println("O carro da placa "+ this.placa + " esta ligando...");
+            this.estaLigado = true;
+        }
+    }
+    public void trocaPlaca(String placa){
+        this.placa = placa;
+    }
+
+    public void desligar(){
+        System.out.println("O carro esta desligando...");
+    }
+
+    public int acelerar(){
+        System.out.println("O carro esta acelarando");
+        this.velocidadeAtual++;
+        return this.velocidadeAtual;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        Carro[] carros = new Carro[2];
 
-        System.out.println("Cadastro de carro");
-        for(int contador = 0; contador < carros.length; contador++ ){
-            Carro novoCarro = new Carro();
+        Carro carro = new Carro();
+        carro.placa = "AYU564";
 
-            System.out.println("Digite a marca do carro");
-            novoCarro.marca =  scanner.next();
+        System.out.println(carro.placa);
 
-            System.out.println("Digite o modelo do carro");
-            novoCarro.modelo =  scanner.next();
+        carro.trocaPlaca("TESTE");
 
-            System.out.println("Digite a cor do carro");
-            novoCarro.cor =  scanner.next();
+        System.out.println(carro.placa);
 
-            System.out.println("Digite a placa do carro");
-            novoCarro.placa =  scanner.next();
+        carro.estaLigado = true;
+        carro.ligar();
 
-
-            System.out.println("Carro cadastrado com sucesso ");
-
-            carros[contador] = novoCarro;
-        }
-
-
-        System.out.println("Esses são seus carros cadastrados");
-        for(int contador = 0; contador < carros.length; contador++ ) {
-            System.out.println(
-                            carros[contador].placa+ " " +
-                            carros[contador].cor + " " +
-                            carros[contador].modelo + " " +
-                            carros[contador].marca
-            );
-        }
+        int velocidadeAtual = carro.acelerar();
+        System.out.println(velocidadeAtual);
 
     }
 }
